37a38927a743bd5b8c35741e33b2acc052a26a33
Rapli24 <mraflyka24@gmail.com>
2025-05-19 02:21:24 +0700

    2

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // <-- tambahkan ini
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable; // <-- tambahkan HasFactory di sini
 
     protected $fillable = [
         'name',
@@ -20,7 +21,6 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    // HAPUS mutator password supaya password disimpan plaintext
     // public function setPasswordAttribute($value)
     // {
     //     $this->attributes['password'] = bcrypt($value);
